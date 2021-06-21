9ec8e166de85b14ae55d9efbdf6ae3368bc2d0c2
priyaganesh6 <51941159+priyaganesh6@users.noreply.github.com>
2021-06-22 04:25:29 +0530

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,8 @@
+const app=require('calc_functions');
+const result=app.add(3,4);
+const res=app.subtract(10,5);
+const ans=app.multiply(10,5);
+console.log(result);
+console.log(res);
+console.log(ans);
+
